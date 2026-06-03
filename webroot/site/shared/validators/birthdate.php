cb39320bb466d14b20e08b263458d1adbce89b75
theoretic <tarasov.alexei@gmail.com>
2026-06-03 18:29:40 +0500

    Refactor birthdate validation logic to check for future dates and remove age restriction checks
    - Commented out the age restriction for a minimum of 17 years.
    - Added validation to ensure the birthdate is not in the future.
    - Updated the date format in the header comment.

--- webroot/site/shared/validators/birthdate.php
+++ webroot/site/shared/validators/birthdate.php
@@ -5,7 +5,7 @@ Includes date.php validation, then checks:
   - age >= 17 years
   - age < 120 years
 AT
-06.05.26
+03.06.26
 */
 
 namespace ProcessWire;
@@ -16,11 +16,17 @@ if( isset($dateError['error']) ) return $dateError;
 
 // $valueTimestamp set by date.php via mktime()
 $now = time();
-$age17  = strtotime('-17 years', $now);
+//$age17  = strtotime('-17 years', $now);
 $age120 = strtotime('-120 years', $now);
 
+/*
 if( $valueTimestamp > $age17 )
 	return [ 'error' => defined('I18N_VALIDATOR')? __('age must be at least 17',I18N_VALIDATOR) : 'возраст должен быть не менее 17 лет' ];
+*/
+
+
+if( $valueTimestamp > time() )
+	return [ 'error' => defined('I18N_VALIDATOR')? __('age must be at least 0',I18N_VALIDATOR) : 'дата рождения не может быть в будущем' ];
 
 if( $valueTimestamp < $age120 )
 	return [ 'error' => defined('I18N_VALIDATOR')? __('age must be less than 120',I18N_VALIDATOR) : 'возраст не может превышать 120 лет' ];
